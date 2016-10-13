5824c445f42985a9acb48159699d3bdd00b1e1cb
GuillaumeLiard <liardg@gmail.com>
2016-10-13 21:18:11 +0200

    anim

--- xr/js/views/game.js
+++ xr/js/views/game.js
@@ -14,5 +14,11 @@ var MainView = Mn.View.extend({
         var inputModel = new InputModel();
         this.showChildView('input', new InputView({model:inputModel}));
         this.showChildView('play', new PlayView({model:inputModel}));
-    }
+    },
+    events:{
+        'click': 'pause'
+    },
+    pause:function(){
+        console.log('pause');
+    },
 });

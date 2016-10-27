00ae8ed81688f99e2c414fe1df4881bc8d6556a7
GuillaumeLiard <liardg@gmail.com>
2016-10-27 19:16:13 +0200

    chariot ok

--- xr/js/behaviors/chariotBehavior.js
+++ xr/js/behaviors/chariotBehavior.js
@@ -1,7 +1,7 @@
 var newX;
 var widthScratch = 480*0.8;
 // var widthChariot;
-var wChariot;
+var wInitChariot;
 var wGame;
 var chariotTl;
 
@@ -10,27 +10,39 @@ var ChariotBehavior = Mn.Behavior.extend({
         chariot:'#chariot',
         path:'#chariot g g path'
     },
+    events:{
+        'resize window': 'resize'
+    },
   modelEvents: {
     'change:x': 'xChanged'
 },
 onAttach:function(){
-    wChariot = this.ui.chariot.width();
-    wGame = this.view.$el.width();
+    wInitChariot = this.ui.chariot.width();
+    // wGame = this.view.$el.width();
 
     this.setPosition();
+    _.bindAll(this,'handleResize');
+    $(window).on('resize',this.handleResize);
     // tl.to(this.ui.chariot, 0, {opacity:1},"intro2")
     // .fromTo(this.ui.path, 1, {fill:color1}, {fill:color5},"intro2");
 },
 setPosition:function(){
+
     chariotTl = new TimelineMax();
-    // chariotTl.to(this.ui.rail, 0, {attr:{x:100, y:-1000}});
-    chariotTl.to(this.ui.chariot, 0, {y:0});
-    // chariotTl.to(this.ui.chariot, 0, {scaleX:2});
-    chariotTl.to(this.ui.chariot, 0.1, {yPercent:230,transformOrigin:"50% 401%"});
+    chariotTl.fromTo(this.ui.path, 0, {fill:color1}, {fill:color5})
+    .to(this.ui.chariot, 0, {left:"50%", top:"70%"})
+    .to(this.ui.chariot, 0, {xPercent:-50, yPercent:-50});
+    this.handleResize();
 },
+handleResize:function(){
+    wGame = this.view.$el.width();
+    var newScale = 0.1*wGame/wInitChariot;
+    chariotTl.to(this.ui.chariot, 0, {scale:newScale});
+},
+
   xChanged: function(event) {
       newX = this.view.model.get('x');
-      var newLeft = 50+(newX*widthScratch/wGame)-((100*wChariot/wGame)/2);
+      var newLeft = 50+(newX*widthScratch/wGame)-((100*wInitChariot/wGame)/2);
       TweenLite.to(this.ui.chariot, 1, {left:newLeft+"%"});
   }
 });

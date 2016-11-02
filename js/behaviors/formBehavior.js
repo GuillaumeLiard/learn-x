94c1dfabb54461d95bd101b5b770c9ba11aa18c4
GuillaumeLiard <liardg@gmail.com>
2016-11-02 20:13:26 +0100

    detect click

--- js/behaviors/formBehavior.js
+++ js/behaviors/formBehavior.js
@@ -6,13 +6,25 @@ var wGame;
 
 module.exports = Mn.Behavior.extend({
     ui:{
-        form:'form'
+        up:'#g3135'
     },
-onAttach:function(){
+    events:{
+        'click @ui.up':'goUp',
+        'mouseover @ui.up':'wannaGoUp',
+    },
 
-    wForm = this.ui.form.width();
-    wGame = this.view.$el.width();
-    var formPos = 50-((100*wForm/wGame)/2);
-    TweenLite.fromTo(this.ui.form, 1, {scale:0.5},{scale:1,left:formPos+"%",opacity:1});
-}
+    goUp:function(){
+        console.log('up');
+    },
+    wannaGoUp:function(){
+        console.log('wannaGoUp');
+    }
+
+// onAttach:function(){
+//
+//     wForm = this.ui.form.width();
+//     wGame = this.view.$el.width();
+//     var formPos = 50-((100*wForm/wGame)/2);
+//     TweenLite.fromTo(this.ui.form, 1, {scale:0.5},{scale:1,left:formPos+"%",opacity:1});
+// }
 });

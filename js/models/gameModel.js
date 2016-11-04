aa599f824fc197ca0f6b2ad8df9f6677e3fca975
GuillaumeLiard <liardg@gmail.com>
2016-11-04 19:21:13 +0100

    small updates

--- js/models/gameModel.js
+++ js/models/gameModel.js
@@ -3,12 +3,13 @@ var Backbone = require('backbone');
 var xMin = -240;
 var xMax = 240;
 module.exports = Backbone.Model.extend({
-  initialize: function() {
-    //   this.set('x',0);
-      console.log('new Model');
-  },
+  // initialize: function() {
+  //   //   this.set('x',0);
+  //     console.log('new Model');
+  // },
   defaults:{
-      'x':0
+      'x':0,
+      'step':10
   },
   validate:function(attrs){
       if(attrs.x>xMax){
